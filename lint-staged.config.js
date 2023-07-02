37571c6f2787e26ca40e6ee7842b25ad80778496
Parsa Ghadimkhani <parsa.ghadimkhani@webjar.ir>
2023-07-02 22:00:29 +0330

    chore: update lint staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,22 @@
+const path = require('path');
+
+const nestSrcEslint = (fileNameList = []) => {
+  const lintFileList = fileNameList
+    .map((file) => path.relative(process.cwd(), file))
+    .filter((file) => file.startsWith('src'));
+  return lintFileList.length
+    ? `eslint --fix ${lintFileList.join(' ')}`
+    : 'echo nothing to lint !';
+};
+
+const nestSrcPrettier = (fileNameList = []) => {
+  const relativeFileList = fileNameList.map((file) =>
+    path.relative(process.cwd(), file).replace(/\\/g, '/'),
+  );
+  return `prettier --write ${relativeFileList.join(' ')}`;
+};
+
 module.exports = {
-  '*.ts': [
-    (filenames) => `npx prettier --write ${filenames.join(' ')}`,
-    (filenames) => `npx eslint --fix ${filenames.join(' ')}`,
-  ],
-  '*': 'npm run build',
+  '*.(js,ts,json,yaml)': nestSrcPrettier,
+  '*.(ts)': [nestSrcEslint, () => 'npx tsc --noEmit'],
 };
