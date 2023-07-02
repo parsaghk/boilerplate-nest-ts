c43db51e5de468ded22572e0adc53fc46a5f6725
Parsa Ghadimkhani <parsa.ghadimkhani@webjar.ir>
2023-07-02 09:20:48 +0330

    chore: add commit lint rules

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,3 +1,35 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+      ],
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'auth',
+        'base',
+        'common',
+        'config',
+        'database',
+        'models',
+        'providers',
+        'shared',
+      ],
+    ],
+  },
 };
