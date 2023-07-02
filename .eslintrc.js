498ca066b889b493dda5be8e8cdfaeb37a81b506
Parsa Ghadimkhani <parsa.ghadimkhani@webjar.ir>
2023-07-02 21:58:16 +0330

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['simple-import-sort', '@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -15,8 +15,15 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', 'lint-staged.config.js'],
   rules: {
+    'simple-import-sort/imports': [
+      'error',
+      {
+        groups: [['^\\u0000', '^@?\\w', '^[^.]', '^\\.']],
+      },
+    ],
+    'simple-import-sort/exports': 'error',
     '@typescript-eslint/no-unused-vars': [
       'error',
       {
@@ -25,12 +32,31 @@ module.exports = {
       },
     ],
     '@typescript-eslint/explicit-member-accessibility': 'error',
-    '@typescript-eslint/lines-between-class-members': 'error',
-    '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/lines-between-class-members': 'error',
+    '@typescript-eslint/member-ordering': 'error',
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'memberLike',
+        modifiers: ['private'],
+        format: ['camelCase'],
+        leadingUnderscore: 'require',
+      },
+      {
+        selector: 'enumMember',
+        format: ['UPPER_CASE'],
+        leadingUnderscore: 'forbid',
+      },
+      {
+        selector: 'variable',
+        format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+        leadingUnderscore: 'forbid',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'error',
     'no-duplicate-imports': 'error',
-    'prettier/prettier': 'error',
   },
 };
